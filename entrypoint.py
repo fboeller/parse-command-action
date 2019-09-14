ff79c291fda9c8ca5cae296eef1047d998c7e57a
fboeller <fabianboeller@googlemail.com>
2019-09-14 20:28:44 +0200

    Output parse results

--- entrypoint.py
+++ entrypoint.py
@@ -8,7 +8,7 @@ command_arguments = sys.argv[2]
 
 try:
     arguments = docopt(specification, argv=command_arguments.split(), help=False, options_first=False)
-    subprocess.run(f'echo ::set-output name=arguments::{command_arguments}'.split())
+    subprocess.run(f'echo ::set-output name=arguments::{arguments}'.split())
     subprocess.run("echo ::set-output name=valid::true".split())
 except DocoptExit:
     subprocess.run("echo ::set-output name=valid::false".split())
